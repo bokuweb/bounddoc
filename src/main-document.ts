2346519c213583858726f670f5a7550df1b4fb35
bokuweb <bokuweb12@gmail.com>
2019-04-16 00:25:49 +0900

    fix deps

--- src/main-document.ts
+++ src/main-document.ts
@@ -1,9 +1,9 @@
 import * as zip from './zip';
 import { xmlFileReader } from './xml-reader';
-import { OOElement } from './xml/nodes';
+// import { OOElement } from './xml/nodes';
 
-import * as reader from './element-reader';
-import { element } from './xml';
+// import * as reader from './element-reader';
+// import { element } from './xml';
 
 export async function readMain(file: zip.Zip, path: string) {
   return await xmlFileReader(file, path);
