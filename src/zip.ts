22d0c98abc9b67ac96d9ccce84b1528f3b4c7cac
bokuweb <bokuweb12@gmail.com>
2019-11-19 16:15:29 +0900

    fix: type (#14)

--- src/zip.ts
+++ src/zip.ts
@@ -1,25 +1,25 @@
-import Zip from 'jszip';
+import JSZip from 'jszip';
 
-export type Zip = Zip;
+export type Zip = JSZip;
 
-export function exists(zip: Zip, name: string) {
+export function exists(zip: JSZip, name: string) {
   return zip.file(name) !== null;
 }
 
-export async function readAsBuf(zip: Zip, name: string) {
+export async function readAsBuf(zip: JSZip, name: string) {
   const array = await zip.file(name).async('uint8array');
   const buf = Buffer.from(array);
   return buf;
 }
 
-export async function readAsUTF8(zip: Zip, name: string) {
+export async function readAsUTF8(zip: JSZip, name: string) {
   const array = await zip.file(name).async('uint8array');
   const buf = Buffer.from(array);
   return buf.toString('utf-8');
 }
 
 export async function createZip(buf: ArrayBuffer) {
-  const zip = new Zip();
+  const zip = new JSZip();
   const z = await zip.loadAsync(buf);
   return z;
 }
